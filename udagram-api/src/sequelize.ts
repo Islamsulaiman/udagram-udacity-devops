882896fe492cd83bb136db42b6db191fd5e2e28f
islam Sulaiman <ma.eslam@yahoo.com>
2022-04-09 23:48:23 +0300

    fixis

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
+export const sequelize = new Sequelize(
+{
   username: config.username,
   password: config.password,
   database: config.database,
@@ -10,4 +11,5 @@ export const sequelize = new Sequelize({
 
   dialect: "postgres",
   storage: ":memory:",
-});
+}
+);
