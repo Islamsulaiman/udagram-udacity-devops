e29ac05e97abc74f52668c4f6c7d3b0b9399345f
islam sulaiman <ma.eslam@yahoo.com>
2022-04-12 08:51:57 +0300

    fixes

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment = {
   production: false,
   appName: 'Udagram',
   // apiHost : process.env.BACKEND_URL
-  // apiHost: 'http://udagram-api-dev22222.us-east-1.elasticbeanstalk.com'
-  apiHost : 'http://localhost:3000/api/v0'
+  apiHost: "http://udagram-api-dev.eba-e6ehqvq2.us-east-1.elasticbeanstalk.com/api/v0"
+  // apiHost : 'http://localhost:3000/api/v0'
 };
 
 /*
